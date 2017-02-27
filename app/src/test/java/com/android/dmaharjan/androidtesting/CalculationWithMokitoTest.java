6a1e49702557aea297266cbcaaccb4625a7c4fd0
crazdrms <dilipmlove@gmail.com>
2017-02-27 14:31:34 +0545

    Another way of mocking objects

--- app/src/test/java/com/android/dmaharjan/androidtesting/CalculationWithMokitoTest.java
+++ app/src/test/java/com/android/dmaharjan/androidtesting/CalculationWithMokitoTest.java
@@ -2,11 +2,14 @@ package com.android.dmaharjan.androidtesting;
 
 import com.android.dmaharjan.util.Calculations;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 /**
@@ -15,17 +18,22 @@ import static org.mockito.Mockito.when;
 
 public class CalculationWithMokitoTest {
 
+    @Rule
+    public MockitoRule mokitoRule = MockitoJUnit.rule(); //initializes annotations and verifies correct use of mockito
+    @Mock
+    private Calculations calculations;
+
     @Test
     public void testSum() {
-        Calculations calculations = mock(Calculations.class);
-        when(calculations.sum(2,3)).thenReturn(5);
-        assertEquals(calculations.sum(2, 3),5);
+        //Calculations calculations = mock(Calculations.class);
+        when(calculations.sum(2, 3)).thenReturn(5);
+        assertEquals(calculations.sum(2, 3), 5);
     }
 
     @Test
     public void testProfit() {
-        Calculations calculations = mock(Calculations.class);
-        when(calculations.profit(300,500)).thenReturn(true);
+        //  Calculations calculations = mock(Calculations.class);
+        when(calculations.profit(300, 500)).thenReturn(true);
         assertTrue(calculations.profit(300, 500));
     }
 
